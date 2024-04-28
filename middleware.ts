d4b70d5eabd2ad553c58166e5d130a2e79806d95
kiu_shubitidze.sandro <shubitidze.sandro@kiu.edu.ge>
2024-04-28 23:13:17 +0400

    Adding Authentication, SideBar

---
+++ middleware.ts
@@ -0,0 +1,23 @@
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+
+const procetcetRoutes = createRouteMatcher([
+  '/',
+  '/credits',
+  '/profile',
+  '/transformations(.*)',
+  '/transformations(.*)/update',
+  '/transformations/add/restore',
+  '/transformations/add/fill',
+  '/transformations/add/remove',
+  '/transformations/add/recolor',
+  '/transformations/add/removeBackground',
+
+])
+
+export default clerkMiddleware((auth, req) => {
+  if(procetcetRoutes(req)) auth().protect();
+});
+
+export const config = {
+  matcher: ["/((?!.+.[w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+};
